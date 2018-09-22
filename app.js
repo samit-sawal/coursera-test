4880f556fba014867f12ed078ab325a8c356d3ea
Samit Sawal <Samit_Sawal@ad.infosys.com>
2018-09-22 19:10:45 +0530

    test filter

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 (function(){
     'use strict';
-    angular.module("nameCalculator",[]).controller("nameCalculatorController",function($scope){
-        $scope.name="";
-        $scope.totalValue = 0;
-        $scope.displayNumeric = function(){
-            $scope.totalValue = calculateNumericForString($scope.name);
+    angular.module("DIApp",[]).controller("DIController",DIController);
+    
+    function DIController($scope,$filter){
+        $scope.name="samit";
 
+        $scope.upper = function(){
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
         }
-
-        function calculateNumericForString(string){
-            var totalStringValue = 0;
-            for(var i=0; i<string.length; i++){
-                totalStringValue += string.charCodeAt(i);
-            }
-
-            return totalStringValue;
-        }
-    });
+    };
 })();
